609fa5a0b486874f2ff6d656e5123e07da924078
Daniel Thomas <me@danthms.com>
2021-02-03 19:45:43 +0000

    flipping tweet link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,7 +14,7 @@ export default function Navigation() {
                     Changelog
                 </NavLink> */}
             </nav>
-            <a href="https://twitter.com/intent/tweet?text=@danthms%20@joshuathorley" target="_blank" className="block text-blue-600 font-bold">Tweet us</a>
+            <a href="https://twitter.com/intent/tweet?text=@danthms%20@joshuathorley" target="_blank" className="block text-purple-600 font-bold">Tweet us</a>
         </header>
     )
 }
